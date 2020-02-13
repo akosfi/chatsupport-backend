61cf04bdcfba07b66f708efa000a2b3eedb2a20e
akosfi <fiakos1997@gmail.com>
2020-02-13 17:56:03 +0100

    added database + models

--- src/db/migrations/20200213154855-create-chat-client.js
+++ src/db/migrations/20200213154855-create-chat-client.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('SupportChatClients', {
+    return queryInterface.createTable('ChatClients', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,6 +25,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('SupportChatClients');
+    return queryInterface.dropTable('ChatClients');
   }
 };

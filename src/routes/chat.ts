39ace644495bffd446d2358f1382d08b4c944850
akosfi <fiakos1997@gmail.com>
2020-03-10 18:29:31 +0100

    users can create/fetch chat clients

--- src/routes/chat.ts
+++ src/routes/chat.ts
@@ -1,28 +1,70 @@
 import express, {Request, Response, NextFunction} from 'express';
 import jwt from 'jsonwebtoken';
-import {User} from '../db/models';
+import _ from 'lodash';
+import {User, ChatClient} from '../db/models';
 
 var router = express.Router();
 
 router.get('/', async (req: Request, res: Response, next: NextFunction) => {
-    const user_token = jwt.decode(req.cookies.token) as { [key: string]: any; };
+    const user = await fetchUserFromRequest(req);
     
-    const user = await User.findOne({where: {id: user_token['id']}});
-
     return res.send({
         clients: user.chatClients 
     });
 });
 
-router.post('/create', (req: Request, res: Response, next: NextFunction) => {
-    //create chat client
+router.post('/create', async (req: Request, res: Response, next: NextFunction) => {
+    const user = await fetchUserFromRequest(req);
+
+    const chatClient = await ChatClient.create({
+        license: Math.floor(Math.random() * 1000000),
+        owner_id: user.id
+    });
+
+    if(chatClient){
+        res.status(200);
+        return res.send({
+            client: {...chatClient}
+        });
+    }
+    else {
+        res.status(404);
+        return res.send({
+            code: 404,
+            message: 'Chat Client cannot be created!',
+        });
+    };
+
     return;
 });
 
-router.get('/{id}', (req: Request, res: Response, next: NextFunction) => {
-    //fetch guests of chat client specified by id
-    return;
+router.get('/{id}', async (req: Request, res: Response, next: NextFunction) => {
+    const user = await fetchUserFromRequest(req);
+
+    //@ts-ignore
+    const chatClient = await _.find(user.chatClients, {'id': req.params.id });
+
+    if(chatClient){
+        res.status(200);
+        return res.send({
+            client: {...chatClient}
+        });
+    }
+    else {
+        res.status(404);
+        return res.send({
+            code: 404,
+            message: 'Chat Client cannot be found!',
+        });
+    };
 });
+
+
+const fetchUserFromRequest = async (req: Request) => {
+    const user_token = jwt.decode(req.cookies.token) as { [key: string]: any; };
+    
+    return await User.findOne({where: {id: user_token['id']}});
+}
 
 
 export {router};

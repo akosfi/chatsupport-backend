1e3a12ed555d62b6a924d4d8d3a35cbf1f5a249e
akosfi <fiakos1997@gmail.com>
2020-04-01 13:04:53 +0200

    auth middleware

--- src/middlewares/auth/authMW.ts
+++ src/middlewares/auth/authMW.ts
@@ -1,19 +1,23 @@
 import jwt from 'jsonwebtoken';
 import {Request, Response, NextFunction} from 'express';
 
-const authMW = (req: Request, res: Response, next: NextFunction) => {
+const authMW = (req, res, next) => {
     if(req.cookies.token){
         jwt.verify(req.cookies.token, "secret", (err: any, decoded: any) => {
             if(err) {
-                res.status(403);
-                return res.end();
+                return res.send({
+                    code: 401,
+                    message: "Unauthorized."
+                });
             }
             return next();
         });
     }
     else {
-        res.status(403);
-        return res.end();
+        return res.send({
+            code: 401,
+            message: "Unauthorized."
+        }); 
     }
 };
 
